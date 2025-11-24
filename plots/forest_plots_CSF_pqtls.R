d733097c65294b310843400707ca42853370cb9b
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2025-11-24 10:52:40 -0500

    Final editorial edits forest_plots_CSF_pqtls.R

--- plots/forest_plots_CSF_pqtls.R
+++ plots/forest_plots_CSF_pqtls.R
@@ -244,18 +244,18 @@ for (i in 1:length(mr_results_list)) {
     y_intercept_line = 12.6
   }
   
-  main <- ggplot(mr_results_comb, aes(x=OR, y=fct_rev(exp_out_ord)), color="black") +
+  main <- ggplot(mr_results_comb, aes(x=OR, y=fct_rev(exp_out_ord))) +
     #Add a reference dashed line at 20
     geom_vline(xintercept = 1, linetype = "longdash", colour = "grey") + 
     #Add dot plot and error bars
     geom_errorbar(aes(xmin = low_CI, xmax = upp_CI), width = 0.25) +
-    geom_point(size = 4, aes(colour = sex)) + 
+    geom_point(size = 4, aes(colour = sex, shape = sex)) + 
     ggtitle("MR results (IVW method)") +
     #Add a line above graph
     geom_hline(yintercept=y_intercept_line, size=2) +
     labs(x="Odds ratio", y = "Exposure") +
     scale_x_continuous(breaks=seq(floor(min(mr_results_comb$low_CI)),ceiling(max(mr_results_comb$upp_CI)),), limits=c(floor(min(mr_results_comb$low_CI)),ceiling(max(mr_results_comb$upp_CI))), expand=c(0,0) ) +
-    scale_shape_manual(values=c(15,16,17,18)) +
+    scale_shape_manual(values=c(16,15,17)) +
     theme_classic(base_size=14) +
     #Remove legend
     #Also remove y-axis line and ticks
@@ -269,7 +269,10 @@ for (i in 1:length(mr_results_list)) {
           axis.line.y = element_blank(),
           axis.ticks.y = element_blank(),
           axis.title.y  = element_blank()
-    ) + guides(shape = "none")
+    ) + guides(shape = "none") +
+    scale_color_manual(values = c("Female-specific" = "#DDAA33", "Male-specific" = "#BB5566", "Sex-combined" = "#004488"),
+                       name = "",
+                       guide = guide_legend(override.aes = list(shape = c(16,15,17), size = 4, stroke = 1.5, colour = c("#DDAA33", "#BB5566", "#004488"))))
   
   or_table <- ggplot(data=mr_results_comb) +
     geom_text(aes(y=fct_rev(exp_out_ord), x=1, label= OR_CI), vjust=0, size = 6) +
@@ -290,9 +293,25 @@ for (i in 1:length(mr_results_list)) {
       plot.title = element_text(hjust =0.5) 
     ) 
   
+  # Format FDR values
+  format_fdr <- function(fdr_value) {
+    if (fdr_value > 0.009) {
+      return(sprintf("%.3f", fdr_value))
+    } else {
+      # Convert to scientific notation with superscript
+      exponent <- floor(log10(fdr_value))
+      value <- fdr_value / 10^exponent
+      return(sprintf("%.2f~x~10^{%d}", value, exponent))
+    }
+  }
+  
+  # Apply formatting to create formatted FDR column
+  mr_results_comb <- mr_results_comb %>%
+    mutate(FDR_formatted = sapply(FDR, format_fdr))
+  
   # to insert the pvalues in scientific format: label = formatC(FDR, format = "e", digits = 2)
   fdr_table <- ggplot(data=mr_results_comb) +
-    geom_text(aes(y=fct_rev(exp_out_ord), x=1, label= formatC(FDR, format = "e", digits = 3)), vjust=0, size = 6) +
+    geom_text(aes(y=fct_rev(exp_out_ord), x=1, label= FDR_formatted), vjust=0, size = 6, parse = TRUE) +
     #Add a line above graph
     geom_hline(yintercept=y_intercept_line, size=2) + 
     ggtitle("FDR") +
@@ -329,11 +348,13 @@ for (i in 1:length(mr_results_list)) {
       plot.title = element_text(hjust =0.5)
     )
   
+  nsnps_title <- expression(paste(italic("n"), " SNPs"))
+  
   nsnps_table <- ggplot(data=mr_results_comb) +
     geom_text(aes(y=fct_rev(exp_out_ord), x=1, label= nsnp), vjust=0, size = 6) +
     #Add a line above graph
     geom_hline(yintercept=y_intercept_line, size=2) + 
-    ggtitle("N SNPs") +
+    ggtitle(nsnps_title) +
     xlab("  ") +
     theme_classic(base_size=14) +
     theme(
@@ -386,9 +407,10 @@ for (i in 1:length(mr_results_list)) {
       plot.title = element_text(hjust = 0.5)
     )
   
-  all <- grid.arrange(disease_table, names_table, nsnps_table, main, or_table, fdr_table, coloc_table, widths=c(5,5,3,8,4.5,3,2))
-  ggsave(here(project_dir, "results", "cis_MR_CSF", "figures", stringr::str_c("forest_plot_neurodegen_CSF_pqtls_", output_suffix,".jpg")), plot = all, width=15, height=7)
-  ggsave(here(project_dir, "results", "cis_MR_CSF", "figures", stringr::str_c("forest_plot_neurodegen_CSF_pqtls_", output_suffix,".pdf")), plot = all, width=17, height=8)
+  all <- grid.arrange(disease_table, names_table, nsnps_table, main, or_table, fdr_table, coloc_table, widths=c(2,5,3,8,4.5,3,2))
+  ggsave(here(project_dir, "results", "cis_MR_CSF", "figures", stringr::str_c("forest_plot_neurodegen_CSF_pqtls_", output_suffix,"_final.jpg")), plot = all, width=15, height=7)
+  ggsave(here(project_dir, "results", "cis_MR_CSF", "figures", stringr::str_c("forest_plot_neurodegen_CSF_pqtls_", output_suffix,"_final.pdf")), plot = all, width=17, height=8)
+  ggsave(here(project_dir, "results", "cis_MR_CSF", "figures", stringr::str_c("forest_plot_neurodegen_CSF_pqtls_", output_suffix,"_final.tif")), plot = all, width=15, height=7, dpi = 300)
 
 }
 
